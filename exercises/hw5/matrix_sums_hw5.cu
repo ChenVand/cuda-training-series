621e18fb38fda0b7107be1c0adfa70241270700b
ChenVand <chenvd@gmail.com>
2025-01-11 15:37:47 +0200

    --

--- exercises/hw5/matrix_sums_hw5.cu
+++ exercises/hw5/matrix_sums_hw5.cu
@@ -30,7 +30,7 @@ __global__ void row_sums(const float *A, float *sums, size_t ds){
 
 __global__ void row_sums_new(const float *A, float *sums, size_t ds){
 
-  __shared__ float partial_sum[32];
+  // __shared__ float partial_sum[32];
   int tid = threadIdx.x;
   int lane = tid % warpSize;
   int warpID = tid / warpSize;
@@ -38,7 +38,7 @@ __global__ void row_sums_new(const float *A, float *sums, size_t ds){
   float val;
   size_t row;
   for (size_t row_batch = 0; row_batch < DSIZE; row_batch+=gridDim.x){
-    if (tid < 32) partial_sum[tid]=0.0f; //A little out of place but should be in this loop
+    // if (tid < 32) partial_sum[tid]=0.0f; //A little out of place but should be in this loop
     row = row_batch + blockIdx.x;
     if (row >= DSIZE) return;
     for (size_t col_batch = 0; col_batch < DSIZE; col_batch+=blockDim.x){
@@ -49,19 +49,18 @@ __global__ void row_sums_new(const float *A, float *sums, size_t ds){
       for (int offset = warpSize/2; offset > 0; offset >>= 1) 
          val += __shfl_down_sync(mask, val, offset);
 
-      // if  (lane == 0) atomicAdd(&partial_sum[warpID], val);
-      if  (lane == 0) partial_sum[warpID] += val;
+      if  (lane == 0) atomicAdd(&sums[row], val);
+      // if  (lane == 0) partial_sum[warpID] += val;
     }
 
-    __syncthreads();
-    if (warpID==0){
-      val = partial_sum[tid];
-      for (int offset = warpSize/2; offset > 0; offset >>= 1) 
-         val += __shfl_down_sync(mask, val, offset);
+    // __syncthreads();
+    // if (warpID==0){
+    //   val = partial_sum[tid];
+    //   for (int offset = warpSize/2; offset > 0; offset >>= 1) 
+    //      val += __shfl_down_sync(mask, val, offset);
 
-      // if  (tid == 0) atomicAdd(&sums[row], val);
-      if  (tid == 0) sums[row] = val;
-    }
+    //   if  (tid == 0) sums[row] = val;
+    // }
   }
 }
 
@@ -104,7 +103,6 @@ int main(){
   if (!validate(h_sums, DSIZE)) return -1; 
   printf("row sums correct!\n");
   cudaMemset(d_sums, 0, DSIZE*sizeof(float));
-  memset(h_sums, 0, DSIZE*sizeof(float));
 
   row_sums_new<<<(DSIZE+block_size-1)/block_size, block_size>>>(d_A, d_sums, DSIZE);
   cudaCheckErrors("kernel launch failure");
